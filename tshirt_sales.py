936cf39f757e4718f62312d26df1829b03ae6d9d
Funkolunk <109599578+Funkolunk@users.noreply.github.com>
2023-01-31 20:40:10 +0100

    Update tshirt_sales.py

--- tshirt_sales.py
+++ tshirt_sales.py
@@ -21,7 +21,7 @@ st.write("### Determine the scenario:")
 price = st.slider('Price of the product?', min_value=3.0, max_value=15.0, value=7.0, step=0.5)
 
 # Advertisment budget
-ads = st.slider('What is the Adv budget?', min_value=35, max_value=65, value=50, step = 2)
+ads = st.slider('What is the advertisment budget?', min_value=35, max_value=65, value=50, step = 2)
 
 # Promotions
 promo = st.slider('What is the promotional budget?', min_value=35, max_value=65, value=45, step = 2)
