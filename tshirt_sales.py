a5d92b101283377e42b7e42a6538effa55c4e97f
Funkolunk <109599578+Funkolunk@users.noreply.github.com>
2023-01-31 20:05:15 +0100

    Update tshirt_sales.py

--- tshirt_sales.py
+++ tshirt_sales.py
@@ -11,7 +11,7 @@ model = st.container()
 
 with header:
 	st.title('Choosing a Marketing Strategy')
-	st.text('This project uses linear regression to establish the relationship between price, advertisement, promotion, and units sold.')
+	st.text("This project uses linear regression to establish the relationship between price, advertisement, \n promotion, and units sold.")
 
 
 st.write("# Predict Sales✨")
