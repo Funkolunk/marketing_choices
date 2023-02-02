8d7ba17c9968442ec145b4b3a19565737b3ff962
Michelle <109599578+Funkolunk@users.noreply.github.com>
2023-02-02 18:07:54 +0100

    Update tshirt_sales.py

--- tshirt_sales.py
+++ tshirt_sales.py
@@ -18,7 +18,7 @@ st.write("# Predict Sales")
 st.write("### Determine the scenario:")
 
 # Price of the product
-price = st.slider('Price of the product in USD?', min_value=3.0, max_value=15.0, value=7.0, step=0.5)
+price = st.slider('Price of the product in USD:', min_value=3.0, max_value=15.0, value=7.0, step=0.5)
 
 # Advertisment budget
 ads = st.slider('What is the advertisment budget in USD?', min_value=35, max_value=65, value=50, step = 2)
